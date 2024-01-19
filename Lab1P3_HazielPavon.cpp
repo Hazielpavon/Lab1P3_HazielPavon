dce6f1dc4a4524188091b36ef78f4d6cb83f2445
Haziel Pavon <hazielpavon93@gmail.com>
2024-01-19 14:17:13 -0600

    Avanzando en el segundo ejercicio

--- Lab1P3_HazielPavon.cpp
+++ Lab1P3_HazielPavon.cpp
@@ -1,21 +1,47 @@
-// Lab1P3_HazielPavon.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
-
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
-    std::cout << "Holis !\n";
+	bool seguir = true;
+
+	while (seguir) {
+		cout << "Ingrese una opcion" << endl;
+		cout << "1. Primer Ejercicio" << endl;
+		cout << "1. Segundo Ejercicio" << endl;
+		cout << "3. Salir" << endl;
+		int opcion;
+		cin >> opcion;
+
+		switch (opcion) {
+		case 1: {
+
+			cout << "Prueba" << endl;
+
+		}
+
+			  break;
+
+		case 2: {
+			cout << "Prueba2" << endl;
+		}
+
+			  break;
+
+		case 3: {
+			seguir = false;
+		}
+
+			  break;
+
+
+
+		}
+
+
+	}
+
+
+
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
